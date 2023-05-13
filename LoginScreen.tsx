38a73477330e1e3ee900918b31d13499f851e74e
Grecia Eravinash <trunghoangnguyendaihoc2021@gmail.com>
2023-05-13 20:15:09 +0700

    add kotlinVersion

--- LoginScreen.tsx
+++ LoginScreen.tsx
@@ -6,8 +6,9 @@
  * @format
  */
 import 'react-native-gesture-handler';
-import React from 'react';
+import React, {useState} from 'react';
 import type { PropsWithChildren } from 'react';
+import SQLite from 'react-native-sqlite-storage';
 import {
     SafeAreaView,
     ScrollView,
@@ -36,8 +37,115 @@ import {
 
 
 function LoginScreen({ navigation }:any): JSX.Element {
+    // const db = SQLite.openDatabase({ name: 'QuanLyChiTieu.db', createFromLocation: '~QuanLyChiTieu.db' });
 
-    const [text, onChangeText] = React.useState('');
+    // db.then((dbInstance) => {
+    //     dbInstance.transaction((tx) => {
+    //         tx.executeSql(
+    //             `CREATE TABLE IF NOT EXISTS account (
+    //             id INTEGER PRIMARY KEY AUTOINCREMENT,
+    //             username TEXT,
+    //             password TEXT
+    //             )`,
+    //             [],
+    //             () => console.log('Table created successfully'),
+    //             (error) => console.log('Error creating table: ', error)
+    //         );
+    //     });
+    // }).catch((error) => console.log('Error opening database: ', error));
+
+    const initializeDatabase = async () => {
+        try {
+          const db = await SQLite.openDatabase({ name: 'QuanLyChiTieu.db', createFromLocation: '~QuanLyChiTieu.db' });
+          return db;
+        } catch (error) {
+          console.log('Error opening database: ', error);
+          return null;
+        }
+      };
+    
+      const createTable = async (db: SQLite.SQLiteDatabase) => {
+        try {
+          await db.transaction((tx) => {
+            tx.executeSql(
+              `CREATE TABLE IF NOT EXISTS Accounts (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                username TEXT,
+                password TEXT,
+                fullname TEXT,
+                birthday TEXT,
+                email TEXT
+              )`,
+              [],
+              () => console.log('Table created successfully'),
+              (error) => console.log('Error creating table: ', error)
+            );
+          });
+        } catch (error) {
+          console.log('Error executing transaction: ', error);
+        }
+      };
+
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    
+    const handleLogin = async () => {
+        const db = await initializeDatabase();
+        if (db) {
+          await createTable(db);
+    
+          // Rest of the login logic
+          // ...
+          if (username && password) {
+            db.transaction((tx) => {
+              tx.executeSql(
+                'SELECT * FROM account WHERE username = ? AND password = ?',
+                [username, password],
+                (_, { rows }) => {
+                  if (rows.length > 0) {
+                    Alert.alert('Login successful');
+                  } else {
+                    Alert.alert('Invalid username or password');
+                  }
+                },
+                (error) => console.log('Error executing query: ', error)
+              );
+            });
+          } else {
+            Alert.alert('Please enter username and password');
+          }
+        }
+      };
+    
+
+      // const addColumns = async (db: SQLite.SQLiteDatabase) => {
+      //   try {
+      //     await db.transaction((tx) => {
+      //       tx.executeSql(
+      //         'ALTER TABLE account ADD COLUMN fullname TEXT;',
+      //         [],
+      //         () => console.log('Column "fullname" added successfully'),
+      //         (error) => console.log('Error adding column "fullname": ', error)
+      //       );
+      //       tx.executeSql(
+      //         'ALTER TABLE account ADD COLUMN birthday TEXT;',
+      //         [],
+      //         () => console.log('Column "birthday" added successfully'),
+      //         (error) => console.log('Error adding column "birthday": ', error)
+      //       );
+      //       tx.executeSql(
+      //         'ALTER TABLE account ADD COLUMN email TEXT;',
+      //         [],
+      //         () => console.log('Column "gmail" added successfully'),
+      //         (error) => console.log('Error adding column "gmail": ', error)
+      //       );
+      //     });
+      //   } catch (error) {
+      //     console.log('Error executing transaction: ', error);
+      //   }
+      // };
+      
+    const [text, onChangeText] = useState('');
 
 
     const Login = () => {
@@ -67,13 +175,14 @@ function LoginScreen({ navigation }:any): JSX.Element {
                             placeholder="Tên đăng nhâp"
                             style={styles.textInput}
                             placeholderTextColor='black'
-
+                            onChangeText={setUsername}
                         />
                         <Text style={{ fontWeight: 'bold', fontSize: 18, color: 'rgba(214, 149, 0, 1)', }}>Mật khẩu:</Text>
                         <TextInput
                             placeholder="Mật khẩu"
                             style={styles.textInput}
                             placeholderTextColor='black'
+                            onChangeText={setPassword}
                         />
 
 
@@ -83,7 +192,7 @@ function LoginScreen({ navigation }:any): JSX.Element {
                                 <Text style={{ color: 'rgba(214, 149, 0, 1)', fontWeight: 'bold' }}> Quên mật khẩu ?</Text>
                             </TouchableOpacity>
                         </View>
-                        <TouchableOpacity style={styles.btnLogin} onPress={Login}>
+                        <TouchableOpacity style={styles.btnLogin} onPress={handleLogin}>
                             <Text style={{ color: 'rgba(0, 151, 126, 1)', fontSize: 18, fontWeight: 'bold' }}>ĐĂNG NHẬP</Text>
                         </TouchableOpacity>
                         <View style={{ flexDirection: 'row', alignSelf: 'center', marginTop: 40 }}>
