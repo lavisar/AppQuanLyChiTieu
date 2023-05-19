d7798390881d9f3c891a77a15f67213099a3ccae
Grecia Eravinash <trunghoangnguyendaihoc2021@gmail.com>
2023-05-19 10:09:51 +0700

    install datetimepicker

--- AddSpendScreen.tsx
+++ AddSpendScreen.tsx
@@ -20,6 +20,7 @@ import {
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import {DatePickerOptions} from '@react-native-community/datetimepicker'
 import {
   Colors,
   DebugInstructions,
